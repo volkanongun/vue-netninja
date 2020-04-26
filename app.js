e9e9566e77812decad7d23036bbd3da9cd7e8dce
Volkan Ongun <me@volkanongun.com>
2020-04-26 19:15:19 +0300

    punchbag lesson 13 end

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 const app = new Vue({
 	el:"#vue-app",
 	data:{
-
+		health: 100,
+		ended: false
 	},
 	template:`
 		<div>
 			<h1>Punchbag Game</h1>
-			
+			<!-- punchbag image -->
+			<div id="bag" v-bind:class="{ burst: ended }"></div>
+
+			<!-- punchbag health -->
+			<div id="bag-health">
+				<div v-bind:style="{width: health + '%'}"></div>
+			</div>
+
+			<!-- game controls -->
+			<div id="controls">
+				<button v-on:click="punch" v-show="!ended">Punch</button>
+				<button v-on:click="restart">Restart</button>
+			</div>
 		</div>
 	`,
 	methods:{
-		
+		punch(){
+			this.health -= 10
+			if(this.health <= 0){
+				this.ended = true;
+			}
+		},
+		restart(){
+			this.health = 100
+			this.ended = false
+		}
 	},
 	computed:{
 		
