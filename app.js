87aec8a934c23a3d273e5deaa6ece987e5b02450
Volkan Ongun <me@volkanongun.com>
2020-04-26 18:01:08 +0300

    v-if ended

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 const app = new Vue({
 	el:"#vue-app",
 	data:{
-		available: false,
-		nearby: false
+		error: false,
+        success: false
 	},
 	template:`
 		<div>
-			<h1>Dynamic CSS</h1>
-			<h2>Example 1</h2>
-			<!-- <div v-on:click="available = !available" v-bind:class="{ available: available }">
-			 	<span>Ryu</span>
-			 </div> -->
-			<h2>Example 2</h2>
-			<button v-on:click="available = !available">Toggle available</button>
-			<button v-on:click="nearby = !nearby">Toggle nearby</button>
-
-			<div v-bind:class="computedClasses">
-				<span>Ryu</span>
-			</div>
+			<h1>Conditionals</h1>
+			<button v-on:click="error = !error">Toggle Error</button>
+			<button v-on:click="success = !success">Toggle Success</button>
+			<!--
+			<p v-if="error">There has been an error</p>
+			<p v-else-if="success">Woo! success</p>
+			-->
+			<p v-show="error">There has been an error</p>
+			<p v-show="success">Whoo, success!</p>
 		</div>
 	`,
 	methods:{
