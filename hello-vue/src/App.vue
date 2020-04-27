82283ef9926acc64e3557978975b759220c03be4
Volkan Ongun <me@volkanongun.com>
2020-04-27 14:27:30 +0300

    local components

---
+++ hello-vue/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <p>{{ title }}</p>
+    <p>{{ greeting() }}</p>
+    <Ninjas></Ninjas>
+  </div>
+</template>
+
+<script>
+import Ninjas from './components/Ninjas.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      title: 'Ninja App',
+    }
+  },
+  components:{
+    Ninjas
+  },
+  methods:{
+    greeting(){
+      return "Greetings!"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-vue/src/components/Ninjas.vue
@@ -0,0 +1,19 @@
+<template>
+	<ul>
+		<li v-for="ninja in ninjas" v-bind:key="ninja">{{ninja}}</li>
+	</ul>
+</template>
+
+<script>
+	export default {
+		data(){
+			return { 
+				ninjas: ['yoshi', 'hattori', 'hanzo']
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
