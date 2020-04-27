cd0311ab3c7410d1d33d37ec1af1f61ae2905039
Volkan Ongun <me@volkanongun.com>
2020-04-27 11:46:53 +0300

    seperation of component data behaviours

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+Vue.component('greeting', {
+	template:'<p>Hey, I\'m a reusable component! {{name}}<button v-on:click="changeName">Change name</button></p>',
+	data(){
+		return {
+			name: 'Yoshi'
+		}
+	},
+	methods:{
+		changeName(){
+			this.name = 'mario'
+		}
+	}
+})
+
 new Vue({
 	el:"#vue-app-one",
 	
 });
 
-
 const two = new Vue({
 	el:"#vue-app-two",
-	
+
 });
