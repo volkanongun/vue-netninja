675e49ecfe4afd107f24a5d328f93f06cca8ea6e
Volkan Ongun <me@volkanongun.com>
2020-04-27 11:37:03 +0300

    lesson start

--- app.js
+++ app.js
@@ -1,44 +1,10 @@
-const one = new Vue({
+new Vue({
 	el:"#vue-app-one",
-	data:{
-		title: "Vue app one"
-	},
-	template:`
-		<div>
-			<h2>{{title}}</h2>
-			<p>{{greet}}</p>
-		</div>
-	`,
-	methods:{
-	},
-	computed:{
-		greet(){
-			return 'Hello from app one :)';
-		}
-	}
+	
 });
 
 
 const two = new Vue({
 	el:"#vue-app-two",
-	data:{
-		title: "Vue app two"
-	},
-	template:`
-		<div>
-			<h2>{{title}}</h2>
-			<p>{{greet}}</p>
-			<button v-on:click="changeTitle">Change app one title</button>
-		</div>
-	`,
-	methods:{
-		changeTitle(){
-			one.title = "Title changed"
-		}
-	},
-	computed:{
-		greet(){
-			return 'Hello from app two :)';
-		}	
-	}
+	
 });
